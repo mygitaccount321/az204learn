2d3bd92cc89cbedda6bc178024cb38657df2a476
vkf1kor <vishwanath.naik2@in.bosch.com>
2024-02-27 12:39:07 +0530

    React

---
+++ basic-frontend-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Student } from './components/Student/Student';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Student></Student>
+    </div>
+  );
+}
+
+export default App;
---
+++ basic-frontend-app/src/components/Student/Student.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react'
+import { Container, Table } from 'react-bootstrap';
+
+export const Student = () => {
+    const [students, setStudents] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try{
+                const response = await fetch("http://localhost:8080/students", { method: 'GET',headers: { "Access-Control-Allow-Origin": "*"}});
+                if(!response.ok){
+                    throw new Error(response.statusText);
+                }
+                const result = await response.json();
+                setStudents(result);
+            } catch(error){
+                console.log(error);
+            }
+        }
+        fetchData();
+    }, []);
+    return (
+        <><h1>Hello Az-204</h1>
+        
+        <Container className="d-flex justify-content-center">
+ <Table striped style={{ width: '100%'}} bordered hover >
+      <thead>
+        <tr>
+          <th>Roll Number</th>
+          <th>Name</th>
+        </tr>
+      </thead>
+      <tbody>
+      {students.map((student, key) => {
+                    return (
+                        <tr key={key}>
+                            <td>{student.rollNumber}</td>
+                            <td>{student.name}</td>
+                        </tr>
+                    )
+                })}
+      </tbody>
+    </Table></Container></>
+    )
+}
